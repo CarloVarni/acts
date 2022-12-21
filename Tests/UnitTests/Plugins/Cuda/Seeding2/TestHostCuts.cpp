c5a18ab376fdf0bd3cee0f40f50f80fbf3caae06
Carlo Varni <carlovarni@MacBook-Pro-di-Carlo-2.local>
2022-12-21 10:07:43 +0100

    format one more time

--- Tests/UnitTests/Plugins/Cuda/Seeding2/TestHostCuts.cpp
+++ Tests/UnitTests/Plugins/Cuda/Seeding2/TestHostCuts.cpp
@@ -46,11 +46,12 @@ TestHostCuts::cutPerMiddleSP(
   std::size_t itLength = std::min(seedCandidates.size(), std::size_t(5));
   // don't cut first element
   for (std::size_t i(1); i < itLength; i++) {
-    float weight = std::get<Acts::CandidatesForSpM<Acts::InternalSpacePoit<TestSpacePoint>>::Components::WEIGHT>(
+    float weight = std::get<Acts::CandidatesForSpM<
+        Acts::InternalSpacePoit<TestSpacePoint>>::Components::WEIGHT>(
         seedCandidates[i]);
-    const auto& bottom =
-      std::get<Acts::CandidatesForSpM<Acts::InternalSpacePoint<TestSpacePoint>>::Components::BSP>(
-            seedCandidates[i]);
+    const auto& bottom = std::get<Acts::CandidatesForSpM<
+        Acts::InternalSpacePoint<TestSpacePoint>>::Components::BSP>(
+        seedCandidates[i]);
     if (weight > 200. or bottom->radius() > 43.) {
       newSeedsVector.push_back(std::move(seedCandidates[i]));
     }
