9d503f9b050099ed9b5172a4c200abe66e155db6
Carlo Varni <carlovarni@MacBook-Pro-di-Carlo-3.local>
2023-06-11 01:46:54 +0200

    test

--- CI/check_pr_labels.py
+++ CI/check_pr_labels.py
@@ -1,5 +1,6 @@
 from github import Github
 import argparse
+import re
 import os
 
 def parse_arguments():
@@ -12,29 +13,22 @@ def parse_arguments():
 
 def whatch_list():
     whatchlist_files = dict()
-    whatchlist_files['CI/physmon/reference/performance_ambi_orthogonal.root'] = ['Changes Performance - Ambiguity resolution']
-    whatchlist_files['CI/physmon/reference/performance_ambi_seeded.root'] = ['Changes Performance - Ambiguity resolution']
-    whatchlist_files['CI/physmon/reference/performance_amvf_orthogonal_hist.root'] = ['Changes Performance - Vertex']
-    whatchlist_files['CI/physmon/reference/performance_amvf_seeded_hist.root'] = ['Changes Performance - Vertex']
-    whatchlist_files['CI/physmon/reference/performance_amvf_truth_estimated_hist.root'] = ['Changes Performance - Vertex']
-    whatchlist_files['CI/physmon/reference/performance_amvf_truth_smeared_hist.root'] = ['Changes Performance - Vertex']
-    whatchlist_files['CI/physmon/reference/performance_ckf_orthogonal.root'] = ['Changes Performance - Finding']
-    whatchlist_files['CI/physmon/reference/performance_ckf_seeded.root'] = ['Changes Performance - Finding']
-    whatchlist_files['CI/physmon/reference/performance_ckf_truth_estimated.root'] = ['Changes Performance - Finding']
-    whatchlist_files['CI/physmon/reference/performance_ckf_truth_smeared.root'] = ['Changes Performance - Finding']
-    whatchlist_files['CI/physmon/reference/performance_gsf.root'] = ['Changes Performance - Fitting']
-    whatchlist_files['CI/physmon/reference/performance_ivf_orthogonal_hist.root'] = ['Changes Performance - Vertex']
-    whatchlist_files['CI/physmon/reference/performance_ivf_seeded_hist.root'] = ['Changes Performance - Vertex']
-    whatchlist_files['CI/physmon/reference/performance_ivf_truth_estimated_hist.root'] = ['Changes Performance - Vertex']
-    whatchlist_files['CI/physmon/reference/performance_ivf_truth_smeared_hist.root'] = ['Changes Performance - Vertex']
-    whatchlist_files['CI/physmon/reference/performance_seeding_orthogonal.root'] = ['Changes Performance - Seeding']
-    whatchlist_files['CI/physmon/reference/performance_seeding_seeded.root'] = ['Changes Performance - Seeding']
-    whatchlist_files['CI/physmon/reference/performance_seeding_truth_estimated.root'] = ['Changes Performance - Seeding']
-    whatchlist_files['CI/physmon/reference/performance_truth_tracking.root'] = ['Changes Performance - Fitting']
-    whatchlist_files['CI/physmon/reference/tracksummary_ckf_orthogonal_hist.root'] = ['Changes Performance - Finding']
-    whatchlist_files['CI/physmon/reference/tracksummary_ckf_seeded_hist.root'] = ['Changes Performance - Finding']
-    whatchlist_files['CI/physmon/reference/tracksummary_ckf_truth_estimated_hist.root'] = ['Changes Performance - Finding']
-    whatchlist_files['CI/physmon/reference/tracksummary_ckf_truth_smeared_hist.root'] = ['Changes Performance - Finding']
+    whatchlist_files['^.github/.*'] = ['Infrastructure']
+    whatchlist_files['^CI/.*'] = ['Infrastructure']
+    whatchlist_files['^cmake/.*'] = ['Infrastructure']
+    whatchlist_files['^Core/.*'] = ['Component - Core']
+    whatchlist_files['^Examples/.*'] = ['Component - Examples']
+    whatchlist_files['^Fatras/.*'] = ['Component - Fatras']
+    whatchlist_files['^Plugins/.*'] = ['Component - Plugins']
+    whatchlist_files['^docs'] = ['Component - Documentation']
+#    whatchlist_files['^CI/physmon/reference.*'] = ['Changes Performance']
+#    whatchlist_files['^CI/physmon/reference/.*_ambi_.*.root'] = ['Changes Performance - Ambiguity resolution']
+#    whatchlist_files['^CI/physmon/reference/.*_amvf_.*.root'] = ['Changes Performance - Vertex']
+#    whatchlist_files['^CI/physmon/reference/.*_ivf_.*.root'] = ['Changes Performance - Vertex']
+#    whatchlist_files['^CI/physmon/reference/.*_ckf_.*.root'] = ['Changes Performance - Finding']
+#    whatchlist_files['^CI/physmon/reference//*_seeding_/*.root'] = ['Changes Performance - Seeding']
+#    whatchlist_files['^CI/physmon/reference/performance_gsf.root'] = ['Changes Performance - Fitting']
+#    whatchlist_files['^CI/physmon/reference/performance_truth_tracking.root'] = ['Changes Performance - Fitting']
     return whatchlist_files
 
 def main():       
@@ -65,33 +59,29 @@ def main():
 
     print('This PR is marked with the following labels:', list_labels)
     #pull.create_issue_comment("this is a test of a comment")
-    pull.add_to_labels("enhancement")
+    #pull.add_to_labels("enhancement")
 
     files = pull.get_files()
     print('List of modified files:')
     for el in files:
-        print(f"   * {el.filename}")
+        print(f" * {el.filename}")
 
-    required_labels = set()
-    for el in files:
-        current_required_labels = whatchlist_files.get(el.filename, [])
-        if len(current_required_labels) == 0:
-            continue
-        for label in current_required_labels:
-            required_labels.add(label)
+    for pattern in whatchlist_files:
+        for filename in files:
+            if not re.search(pattern, filename):
+                continue
+            # get the required labels for this pattern
+            toadd_labels = whatchlist_files[pattern]
+            # add label to PR if missing
+            for label in toadd_labels:
+                if label not in list_labels:
+                    list_labels.add(label)
+                    pull.add_to_labels(label)
+            break
 
-    missing_labels = set()
-    for required_label in required_labels:
-        if required_label in list_labels:
-            continue
-        missing_labels.add(required_label)
-
-    if len(missing_labels) != 0:
-        print(f"Available Labels: {list_labels}")
-        print(f"Required Labels: {required_labels}")
-        print(f"Missing Labels: {missing_labels}")
-        pull.create_issue_comment(f"This PR modifies the performance of a component but it does not contain the required label. Please add: {missing_labels}")
-        raise Exception(f"This PR modifies the performance of a component but it does not contain the required label")
+    if 'Changes Performance' in list_labels:
+        pull.create_issue_comment(f"This PR modifies the performance of a component")
+        #raise Exception(f"This PR modifies the performance of a component but it does not contain the required label")
 
 if __name__ == "__main__":
     main()
