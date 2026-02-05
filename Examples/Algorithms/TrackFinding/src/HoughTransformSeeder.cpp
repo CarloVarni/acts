ce63e3ac04f6b945e4c2c4e480e0b4fe49f65832
Krzysztof Cieśla <krzysztof.marcin.ciesla@cern.ch>
2026-02-05 12:33:45 +0100

    Fix bin filling

--- Examples/Algorithms/TrackFinding/src/HoughTransformSeeder.cpp
+++ Examples/Algorithms/TrackFinding/src/HoughTransformSeeder.cpp
@@ -233,7 +233,7 @@ ActsExamples::ProcessCode ActsExamples::HoughTransformSeeder::execute(
         if (int entries = m_houghHist.nLayers(y, x); entries > 0) {
           ACTS_DEBUG(std::format("bin (q/pT, phi) = ({}, {})", y, x));
           // Flat layers
-          // hh_hist->SetBinContent(y, x, entries);
+          // hh_hist->SetBinContent(y + 1, x + 1, entries);
 
           // Bit pattern
           const std::uint16_t bits = std::accumulate(
@@ -241,7 +241,7 @@ ActsExamples::ProcessCode ActsExamples::HoughTransformSeeder::execute(
               std::uint16_t{}, [](std::uint16_t sum, std::uint16_t layer) {
                 return sum | 0x1 << layer;
               });
-          hough_hist->SetBinContent(y, x, bits);
+          hough_hist->SetBinContent(y + 1, x + 1, bits);
           ACTS_DEBUG(std::format("bitmask={} n_bits={}",
                                  std::bitset<16>(bits).to_string(), entries));
 
@@ -332,7 +332,7 @@ ActsExamples::ProcessCode ActsExamples::HoughTransformSeeder::execute(
 
     const auto all_peaks = slidingWindowPeaks(m_houghHist, m_cfg.slidingWindow);
     for (const auto& peak : all_peaks) {
-      peaks_hist->Fill(m_bins_y[peak[0] - 1], peak[1]);
+      peaks_hist->Fill(m_bins_y[peak[0]], peak[1]);
     }
 
     m_writer->writeObj(hough_hist.get());
